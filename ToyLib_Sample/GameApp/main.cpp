704b72064f55e23bdf0842c057f8c57925a1efa8
Daisuke <daisukn@outlook.com>
2025-03-24 06:23:49 +0900

    first commt

---
+++ ToyLib_Sample/GameApp/main.cpp
@@ -0,0 +1,25 @@
+#include "Application.h"
+//#include "SingleInstance.h"
+#include <memory>
+
+
+int main(int argc, char** argv)
+{
+/*
+    SingleInstance instance;
+    if (!instance.IsLocked())
+    {
+        return 1;
+    }
+*/
+    
+    std::unique_ptr<Application> app = std::make_unique<Application>();
+    
+    if (app->Initialize())
+    {
+        app->RunLoop();
+    }
+    app->Shutdown();
+    return 0;
+}
+
